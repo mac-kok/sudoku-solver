a6ec7a08b16343be5b53757b5bef38f57e56477f
Maartje <mac.kok@outlook.com>
2022-09-26 11:17:47 +0200

    build(bootstrap): voegt bootstrap toe aan het project

--- template/index.php
+++ template/index.php
@@ -2,6 +2,7 @@
 <html lang="en">
     <head>
         <title>Sudoku Solver</title>
+        <link href="../public/bootstrap.min.css" rel="stylesheet" type="text/css">
         <link href="../public/style.css" rel="stylesheet" type="text/css">
     </head>
     <body>
