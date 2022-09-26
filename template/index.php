b7ab5943639fbda8e5a3ea51084dfe208da18432
Maartje <mac.kok@outlook.com>
2022-09-26 11:02:46 +0200

    build(style): voegt index en stylesheet toe

---
+++ template/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Sudoku Solver</title>
+        <link href="../public/style.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        
+    </body>
+</html>
