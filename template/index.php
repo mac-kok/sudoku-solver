b0e51d8b112bc87c5aab822b899a6adb14e3cdea
Maartje <mac.kok@outlook.com>
2022-09-26 12:43:54 +0200

    style: voegt basislayout toe

--- template/index.php
+++ template/index.php
@@ -2,10 +2,15 @@
 <html lang="en">
     <head>
         <title>Sudoku Solver</title>
-        <link href="../public/bootstrap.min.css" rel="stylesheet" type="text/css">
-        <link href="../public/style.css" rel="stylesheet" type="text/css">
+        <link href="../public/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+        <link href="../public/css/style.css" rel="stylesheet" type="text/css">
     </head>
     <body>
-        
+    <div class="container text-center">
+        <div class="row m-4">
+            <h1>Sudoku Solver</h1>
+        </div>
+        <?php include 'sudoku/sudoku.php' ?>
+    </div>
     </body>
 </html>
---
+++ template/sudoku/sudoku.php
@@ -0,0 +1,23 @@
+<?php
+    $nonet = 'nonet.php';
+?>
+
+<div class="row justify-content-center">
+    <div class="col-auto border border-dark">
+        <div class="row justify-content-center">
+            <?php include $nonet ?>
+            <?php include $nonet ?>
+            <?php include $nonet ?>
+        </div>
+        <div class="row justify-content-center">
+            <?php include $nonet ?>
+            <?php include $nonet ?>
+            <?php include $nonet ?>
+        </div>
+        <div class="row justify-content-center">
+            <?php include $nonet ?>
+            <?php include $nonet ?>
+            <?php include $nonet ?>
+        </div>
+    </div>
+</div>
