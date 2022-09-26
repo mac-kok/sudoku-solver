31ffd8c449c96d2becd579592ea976def4254c53
Maartje <mac.kok@outlook.com>
2022-09-26 13:13:17 +0200

    style: maakt form van layout

--- template/sudoku/sudoku.php
+++ template/sudoku/sudoku.php
@@ -2,22 +2,29 @@
     $nonet = 'nonet.php';
 ?>
 
-<div class="row justify-content-center">
-    <div class="col-auto border border-dark">
-        <div class="row justify-content-center">
-            <?php include $nonet ?>
-            <?php include $nonet ?>
-            <?php include $nonet ?>
-        </div>
-        <div class="row justify-content-center">
-            <?php include $nonet ?>
-            <?php include $nonet ?>
-            <?php include $nonet ?>
-        </div>
-        <div class="row justify-content-center">
-            <?php include $nonet ?>
-            <?php include $nonet ?>
-            <?php include $nonet ?>
+<form method="post">
+    <div class="row justify-content-center">
+        <div class="col-auto border border-dark">
+            <div class="row justify-content-center">
+                <?php include $nonet ?>
+                <?php include $nonet ?>
+                <?php include $nonet ?>
+            </div>
+            <div class="row justify-content-center">
+                <?php include $nonet ?>
+                <?php include $nonet ?>
+                <?php include $nonet ?>
+            </div>
+            <div class="row justify-content-center">
+                <?php include $nonet ?>
+                <?php include $nonet ?>
+                <?php include $nonet ?>
+            </div>
         </div>
     </div>
-</div>
+    <div class="row justify-content-center mt-4">
+        <div class="col-auto">
+            <button type="submit" name="solve" class="btn btn-primary">Solve</button>
+        </div>
+    </div>
+</form>
